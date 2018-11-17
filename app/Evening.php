834455565392cca6050b42234a291c41fefb38e6
kj1471 <38695015+kj1471@users.noreply.github.com>
2018-11-17 16:15:32 +0000

    Pull Masters Database to Frontend (#2)
    * Create Venue Model and Migration
    
    * Create Evening model and migration and relate to Venue
    
    * Create Entry model and migration and relation to evenings and venues
    
    * Create leave model and migration and relate to evening and venue
    
    * Update README

---
+++ app/Evening.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Evening extends Model
+{
+    public function venue()
+    {
+    	return $this->belongsTo('App\Venue');
+    }
+
+    public function entries()
+    {
+    	return $this->hasMany('App\Entry');
+    }
+
+    public function leaves()
+    {
+    	return $this->hasMany('App\Leave');
+    }
+}
---
+++ app/Venue.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Venue extends Model 
+{
+    public function evenings()
+    {
+    	return $this->hasMany('App\Evening');
+    }
+
+    public function entries()
+    {
+    	return $this->hasManyThrough('App\Entry', 'App\Evening');
+    }
+
+    public function leaves()
+    {
+    	return $this->hasManyThrough('App\Leave', 'App\Evening');
+    }
+}

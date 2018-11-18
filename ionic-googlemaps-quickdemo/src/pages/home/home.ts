b59765fc8b2e20b544a96a44d6aafea7875496c2
PrismaticPolygon <domwhite97@gmail.com>
2018-11-18 13:42:00 +0000

    Pre-test commit; all pages, providers, greatly improved

--- ionic-googlemaps-quickdemo/src/pages/home/home.ts
+++ ionic-googlemaps-quickdemo/src/pages/home/home.ts
@@ -1,15 +1,12 @@
 import {ChangeDetectorRef, Component} from '@angular/core';
-import { ToastController } from 'ionic-angular';
 import {
   GoogleMaps,
   GoogleMap,
   GoogleMapsEvent,
-  Marker,
-  GoogleMapsAnimation,
-  MyLocation, GoogleMapOptions, LatLngBounds, Environment, HtmlInfoWindow
+  GoogleMapOptions,
+  Environment
 } from '@ionic-native/google-maps';
-import {Club, ClubProvider} from "../../providers/club/club";
-import {ProgressBarComponent} from "angular-progress-bar";
+import {Club, ClubProvider, CustomGoogleMapMarker} from "../../providers/club/club";
 
 @Component({
   selector: 'page-home',
@@ -19,26 +16,10 @@ export class HomePage {
 
   map: GoogleMap;
   selectedClub: Club;
-  base64URL: string;
 
-  constructor(public toastCtrl: ToastController, public clubProvider: ClubProvider, private changeDetectorRef: ChangeDetectorRef) {
-  }
+  constructor(public clubProvider: ClubProvider, private changeDetectorRef: ChangeDetectorRef) {}
 
   ionViewDidLoad() {
-
-    // this.clubProvider.getClubs().then((clubs: Club[]) => {
-    //
-    //   // this.selectedClub = clubs[2];
-    //
-    //   this.base64URL = this.clubProvider.getMarkerImage(clubs[0]);
-    //
-    // });
-
-    this.loadMap();
-
-  }
-
-  loadMap() {
 
     Environment.setEnv({
       'API_KEY_FOR_BROWSER_RELEASE': '"AIzaSyBygBnRpARdnUWNBg1NR9Je4yfe2TinpMs"',
@@ -48,81 +29,81 @@ export class HomePage {
     let mapOptions: GoogleMapOptions = {
 
       camera: {
-        target: {
-          lat: 54.775997,
-          lng: -1.5740014
-        },
-        zoom: 18,
+        // target: {
+        //   lat: 54.775997,
+        //   lng: -1.5740014
+        // },
+        zoom: 10,
         tilt: 30
       },
       styles: [
-      {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
+        {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
         //{elementType: 'labels', stylers: [{visibility: 'off'}]},
-      {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
-      {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
+        {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
+        {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
         {featureType: 'landscape.man_made',
           elementType: 'all',
           stylers: [{visibility: 'off'}]},
-      {
-        featureType: 'poi',
-        elementType: 'all',
-        stylers: [{visibility: 'off'}]
-      }, {
-        featureType: 'road',
-        elementType: 'geometry',
-        stylers: [{color: '#38414e'}]
-      },
-      {
-        featureType: 'road',
-        elementType: 'geometry.stroke',
-        stylers: [{color: '#212a37'}]
-      },
-      {
-        featureType: 'road',
-        elementType: 'labels.text.fill',
-        stylers: [{color: '#9ca5b3'}]
-      },
-      {
-        featureType: 'road.highway',
-        elementType: 'geometry',
-        stylers: [{color: '#746855'}]
-      },
-      {
-        featureType: 'road.highway',
-        elementType: 'geometry.stroke',
-        stylers: [{color: '#1f2835'}]
-      },
-      {
-        featureType: 'road.highway',
-        elementType: 'labels.text.fill',
-        stylers: [{color: '#f3d19c'}]
-      },
-      {
-        featureType: 'transit',
-        elementType: 'geometry',
-        stylers: [{color: '#2f3948'}]
-      },
-      {
-        featureType: 'transit.station',
-        elementType: 'labels.text.fill',
-        stylers: [{color: '#d59563'}]
-      },
-      {
-        featureType: 'water',
-        elementType: 'geometry',
-        stylers: [{color: '#17263c'}]
-      },
-      {
-        featureType: 'water',
-        elementType: 'labels.text.fill',
-        stylers: [{color: '#515c6d'}]
-      },
-      {
-        featureType: 'water',
-        elementType: 'labels.text.stroke',
-        stylers: [{color: '#17263c'}]
-      }
-    ],
+        {
+          featureType: 'poi',
+          elementType: 'all',
+          stylers: [{visibility: 'off'}]
+        }, {
+          featureType: 'road',
+          elementType: 'geometry',
+          stylers: [{color: '#38414e'}]
+        },
+        {
+          featureType: 'road',
+          elementType: 'geometry.stroke',
+          stylers: [{color: '#212a37'}]
+        },
+        {
+          featureType: 'road',
+          elementType: 'labels.text.fill',
+          stylers: [{color: '#9ca5b3'}]
+        },
+        {
+          featureType: 'road.highway',
+          elementType: 'geometry',
+          stylers: [{color: '#746855'}]
+        },
+        {
+          featureType: 'road.highway',
+          elementType: 'geometry.stroke',
+          stylers: [{color: '#1f2835'}]
+        },
+        {
+          featureType: 'road.highway',
+          elementType: 'labels.text.fill',
+          stylers: [{color: '#f3d19c'}]
+        },
+        {
+          featureType: 'transit',
+          elementType: 'geometry',
+          stylers: [{color: '#2f3948'}]
+        },
+        {
+          featureType: 'transit.station',
+          elementType: 'labels.text.fill',
+          stylers: [{color: '#d59563'}]
+        },
+        {
+          featureType: 'water',
+          elementType: 'geometry',
+          stylers: [{color: '#17263c'}]
+        },
+        {
+          featureType: 'water',
+          elementType: 'labels.text.fill',
+          stylers: [{color: '#515c6d'}]
+        },
+        {
+          featureType: 'water',
+          elementType: 'labels.text.stroke',
+          stylers: [{color: '#17263c'}]
+        }
+      ],
       controls: {
         myLocation: true,
       },
@@ -132,63 +113,18 @@ export class HomePage {
       }
 
     };
-
     this.map = GoogleMaps.create('map_canvas', mapOptions);
 
-    this.clubProvider.getClubs().then((clubs: Club[]) => {
-
-      console.log(clubs);
+    this.map.getMyLocation({enableHighAccuracy: true})
+      .then((location) => this.map.setCameraTarget(location.latLng))
+      .then(() => this.clubProvider.getClubs())
+      .then((clubs: Club[]) => {
 
         clubs.forEach(club => {
 
-          // let htmlInfoWindow = new HtmlInfoWindow();
-          //
-          // let frame: HTMLElement = document.createElement("div");
-          //
-          // let progressBar = ProgressBarComponent;
-          //
-          // frame.innerHTML = progressBar.toString();
-          //
-          // htmlInfoWindow.setContent(progressBar.toString(), {width: "80", height: "80"});
+          let marker: CustomGoogleMapMarker = new CustomGoogleMapMarker(this.map, club);
 
-
-          // frame.innerHTML = '<h3>{{club.name}}</h3>'
-          //
-          // frame.getElementsByTagName("img")[0].addEventListener("click", () => htmlInfoWindow.setBackgroundColor('red'));
-          //
-          // htmlInfoWindow.setContent(frame, {
-          //   width: "80",
-          //   height: "80"
-          // });
-
-          // I'd have to output the makrer.
-
-          // Right. I'll just cheat. How hard can it be?
-
-
-          let marker: Marker = this.map.addMarkerSync({
-            title: club.name,
-            icon: this.clubProvider.getMarkerImage(club),
-            animation: 'DROP',
-            position: {
-              lat: club.latitude,
-              lng: club.longitude
-            }
-          });
-
-          this.clubProvider.setWatch(club, marker);
-
-          // htmlInfoWindow.open(marker);
-
-          //marker.setIcon()
-
-          //  url?: string;
-          //     size?: {
-          //         width?: number;
-          //         height?: number;
-          //     };
-
-          // Need to refresh.
+          this.map.addMarkerSync(marker);
 
           marker.on(GoogleMapsEvent.MARKER_CLICK).subscribe(() => {
 
@@ -200,59 +136,8 @@ export class HomePage {
 
         });
 
-    }).then(() => this.clubProvider.getFurthestPair()).then((bounds) => {
-
-      //console.log(bounds.toString());
-
-      //this.map.setCameraTarget(bounds.getCenter())
-
-    });
-
-  }
-
-  onButtonClick() {
-    this.map.clear();
-
-    // Get the location of you
-    this.map.getMyLocation()
-      .then((location: MyLocation) => {
-        console.log(JSON.stringify(location, null ,2));
-
-        // What's going wrong?
-
-        // Move the map camera to the location with animation
-        this.map.animateCamera({
-          target: location.latLng,
-          zoom: 17,
-          tilt: 30
-        })
-        .then(() => {
-          // add a marker
-          let marker: Marker = this.map.addMarkerSync({
-            title: '@ionic-native/google-maps plugin!',
-            snippet: 'This plugin is awesome!',
-            position: location.latLng,
-            animation: GoogleMapsAnimation.BOUNCE
-          });
-
-          // show the infoWindow
-          marker.showInfoWindow();
-
-          // If clicked it, display the alert
-          marker.on(GoogleMapsEvent.MARKER_CLICK).subscribe(() => {
-            this.showToast('clicked!');
-          });
-        });
       });
+
   }
 
-  showToast(message: string) {
-    let toast = this.toastCtrl.create({
-      message: message,
-      duration: 2000,
-      position: 'middle'
-    });
-
-    toast.present(toast);
-  }
 }
--- ionic-googlemaps-quickdemo/src/providers/club/club.ts
+++ ionic-googlemaps-quickdemo/src/providers/club/club.ts
@@ -1,10 +1,10 @@
-import {ChangeDetectorRef, Injectable, NgZone} from '@angular/core';
+import { Injectable, NgZone} from '@angular/core';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/toPromise';
-import {LatLng, LatLngBounds, Marker, MarkerIcon} from "@ionic-native/google-maps";
 import * as moment from 'moment';
-import {Observable} from "rxjs";
+import {Observable, Subscription} from "rxjs";
+import {GoogleMap, LatLng, Marker} from "@ionic-native/google-maps";
 
 @Injectable()
 export class ClubProvider {
@@ -12,8 +12,6 @@ export class ClubProvider {
   clubs: Club[];
 
   constructor(public http: Http, public ngZone: NgZone) {}
-
-
 
   public getClubs(): Promise<Club[]> {
 
@@ -28,187 +26,8 @@ export class ClubProvider {
       return Promise.resolve(this.clubs);
 
     }
-  }
-
-  public setWatch(club: Club, marker: Marker) {
-
-      club.marker = marker;
-
-      Observable.interval(500)
-        .flatMap(() => this.http.get("http://10.245.1.242:8000/count/" + club.id))
-        .map((response: any) => response.json())
-        .startWith(club.count)
-        .distinctUntilChanged()
-        // .map((count: number) => Math.floor((count / club.capacity) * 100))
-        .subscribe(next => {
-
-          console.log(club.name + ": " + next);
-
-          this.ngZone.run(() => club.count = next);
-
-          club.marker.setIcon({url: this.getMarkerImage(club)});
-
-        });
 
   }
-
-
-  // Now, I have to update things.
-
-  public getMarkerImage(club: Club) {
-
-    let canvas: HTMLCanvasElement = document.createElement("canvas");
-    let percent: number = club.count / club.capacity;
-    let ctx: CanvasRenderingContext2D = canvas.getContext('2d');
-    let size: number = 80;
-
-    ctx.canvas.width = size;
-    ctx.canvas.height = size;
-
-    let cw = size / 2;
-    let ch = size / 2;
-    let diff = percent * Math.PI * 2;
-
-    ctx.clearRect(0, 0, size, size);
-    ctx.beginPath();
-    ctx.arc(cw, ch, size / 2, 0, 2 * Math.PI, false);
-
-    ctx.fillStyle = '#f4f4f4';
-    ctx.fill();
-    ctx.strokeStyle = "#e7f2ba";
-    ctx.stroke();
-    ctx.closePath();
-    ctx.clip();
-
-    // let img = new Image();
-    //
-    // img.addEventListener('load', () => {
-    //
-    //   console.log("Onload event listener trigger");
-    //
-    //   ctx.drawImage(img, 0, 0, size, size);
-    //
-    // });
-    //
-    // img.src = "assets/imgs/hearst_castle.jpg";
-    //
-    // img.onload = (event) => {
-    //
-    //   console.log("Onload triggered");
-    //
-    //   ctx.drawImage(img, 0, 0);
-    //
-    //
-    // };
-
-    // img.src = "assets/icon/favicon.ico";
-
-    //
-    //
-    if (percent < 0.6) {
-
-      ctx.strokeStyle = '#66FF00'
-
-    } else if (percent < 0.8) {
-
-      ctx.strokeStyle =  '#FFFF00';
-
-    } else {
-
-      ctx.strokeStyle = '#FF0000';
-
-    }
-
-    let offset = - (Math.PI / 2);
-
-
-    ctx.fillStyle = '#000';
-    ctx.textAlign = 'center';
-    ctx.lineWidth = 15;
-    ctx.font = '10pt verdana';
-    ctx.beginPath();
-
-    ctx.lineWidth = size / 5;
-    ctx.arc(cw, ch, size / 2, offset, diff + offset, false);
-
-    ctx.stroke();// Stroke function
-    //
-
-    // let lines: string[] = this.getLines(ctx, club.name, size - (2 * size / 5));
-    //
-    // for (let n = 0; n < lines.length; n++) {
-    //
-    //   ctx.fillText(lines[n], cw, ch + n * 10);
-    //
-    // }
-    //
-    // console.log(lines);
-
-
-    // this.wrapText(ctx, club.name, cw, ch, size - (2 * size / 5), 15);
-
-    ctx.fillText(club.name, cw, ch); //text value & text position
-
-    let base64URL: string = canvas.toDataURL('bas64');
-
-    return base64URL;
-  }
-
-  public getLines(ctx, text, maxWidth) {
-
-    let words = text.split(" ");
-    let lines = [];
-    let currentLine = words[0];
-
-    for (let i = 1; i < words.length; i++) {
-
-      let word = words[i];
-      let width = ctx.measureText(currentLine + " " + word).width;
-
-      if (width < maxWidth) {
-        currentLine += " " + word;
-      } else {
-        lines.push(currentLine);
-        currentLine = word;
-      }
-    }
-
-    lines.push(currentLine);
-
-    return lines;
-  }
-
-  public wrapText(context, text, x, y, maxWidth, lineHeight) {
-
-      let words = text.split(" ");
-      let line = "";
-
-      // Yeah, modify this function. Pass in the centre value.
-
-      for(let n = 0; n < words.length; n++) {
-
-        let testLine = line + words[n] + ' ';
-        let metrics = context.measureText(testLine);
-        let testWidth = metrics.width;
-        if (testWidth > maxWidth && n > 0) {
-
-          context.fillText(line, x, y);
-          line = words[n] + ' ';
-          y += lineHeight;
-
-        } else {
-
-          line = testLine;
-
-        }
-    }
-
-    context.fillText(line, x, y);
-
-  }
-
-
-  // Most importantly: my custom radial icon
 
   public getChargeString(club: Club): string {
 
@@ -226,67 +45,157 @@ export class ClubProvider {
 
   }
 
-  // Hmmm. Won't quite work. I'd have to generate the marker_icon on update.
-  // Presumably I can export the progress bar...? I'm seriously constrained by Google Maps
-  // Perhaps the observable isn't being destroyed? Or updated? Or something?
+}
 
-  // public getCapacityPercentage(club: Club): Observable<number> {
-  //
-  //   return Observable.interval(2000)
-  //     .flatMap(() => this.http.get("http://10.245.1.242:8000/count/" + club.id))
-  //     .map((response: any) => response.json())
-  //     .startWith(club.count)
-  //     // .do((value) => console.log(value))
-  //     // .do(() => this.ngZone.run(() => {}))
-  //     .map((count: number) => Math.floor((count / club.capacity) * 100))
-  //     .distinctUntilChanged()
-  //     .do((value))
-  //
-  // }
+export class Club {
 
-  // slash/count/id
+  private _id: number;
+  private _count: number;
+  private _capacity: number;
+  private _charge_time: string;
+  private _charge_cost: number;
+  private _image: string;
+  private _latitude: number;
+  private _longitude: number;
+  private _name: string;
 
-  public getFurthestPair(): Promise<LatLngBounds> {
+  private _onChanges: Observable<number>;
 
-    return Promise.resolve(null);
+  constructor(private http: Http) {
 
-     // return this.getClubs().then(clubs => {
-     //
-     //   console.log(clubs);
-     //
-     //   let maxLongitude, minLongitude = clubs[0].longitude;
-     //   let maxLatitude, minLatitude = clubs[0].latitude;
-     //
-     //   clubs.forEach(club => {
-     //
-     //     if (club.latitude > maxLatitude) maxLatitude = club.latitude;
-     //     if (club.latitude < minLatitude) minLatitude = club.latitude;
-     //     if (club.longitude > maxLongitude) maxLongitude = club.longitude;
-     //     if (club.longitude > minLongitude) minLongitude = club.longitude;
-     //
-     //   });
-     //
-     //   console.log(maxLongitude, maxLatitude, minLongitude, minLatitude);
-     //
-     //   return Promise.resolve(new LatLngBounds([new LatLng(maxLatitude, maxLongitude), new LatLng(minLatitude, minLongitude)]));
-     //
-     //  });
+    this._onChanges = Observable.interval(500)
+      .flatMap(() => this.http.get("http://10.245.1.242:8000/count/" + this._id))
+      .map((response: any) => response.json())
+      .startWith(this._count)
+      .distinctUntilChanged()
 
+  }
+
+  get id(): number {
+    return this._id;
+  }
+
+  get count(): number {
+    return this._count;
+  }
+
+  get capacity(): number {
+    return this._capacity;
+  }
+
+  get image(): string {
+    return this._image;
+  }
+
+  get latitude(): number {
+    return this._latitude;
+  }
+
+  get longitude(): number {
+    return this._longitude;
+  }
+
+  get name(): string {
+    return this._name;
+  }
+
+  get charge_time(): string {
+    return this._charge_time;
+  }
+
+  get charge_cost(): number {
+    return this._charge_cost;
+  }
+
+  get onChanges(): Observable<number> {
+    return this._onChanges;
   }
 
 }
 
-export type Club = {
-  capacity: number,
-  charge_cost: number,
-  charge_time: string,
-  count: number,
-  id: number,
-  image: string,
-  latitude: number,
-  logo: string,
-  longitude: number,
-  name: string,
-  marker: Marker,
-  place_id: string
-};
+export class CustomGoogleMapMarker extends Marker {
+
+  static size = 80;
+
+  club: Club;
+  subscription: Subscription;
+  position: LatLng;
+
+  constructor(map: GoogleMap, club: Club) {
+
+    super(map, {});
+
+    this.club = club;
+    this.setTitle(this.club.name);
+    this.setAnimation("BOUNCE");
+    this.position = new LatLng(this.club.latitude, this.club.longitude);
+
+    this.subscription = this.club.onChanges.subscribe((count: number) => {
+
+      this.setIcon(this.generateMarkerIcon(count));
+
+    });
+
+  }
+
+  private generateMarkerIcon(count: number): string {
+
+    let canvas: HTMLCanvasElement = document.createElement("canvas");
+    let percent: number = count / this.club.capacity;
+    let ctx: CanvasRenderingContext2D = canvas.getContext('2d');
+
+    ctx.canvas.width = CustomGoogleMapMarker.size;
+    ctx.canvas.height = CustomGoogleMapMarker.size;
+    let cw = CustomGoogleMapMarker.size / 2;
+    let ch = CustomGoogleMapMarker.size / 2;
+    let diff = percent * Math.PI * 2;
+
+    ctx.clearRect(0, 0, CustomGoogleMapMarker.size, CustomGoogleMapMarker.size);
+    ctx.beginPath();
+    ctx.arc(cw, ch, CustomGoogleMapMarker.size / 2, 0, 2 * Math.PI, false);
+
+    ctx.fillStyle = '#f4f4f4';
+    ctx.fill();
+    ctx.strokeStyle = "#e7f2ba";
+    ctx.stroke();
+    ctx.closePath();
+    ctx.clip();
+
+    let getStrokeStyle = (percent: number) => {
+
+      if (percent < 0.6) {
+
+        return '#66FF00'
+
+      } else if (percent < 0.8) {
+
+        return '#FFFF00';
+
+      } else {
+
+        return '#FF0000';
+
+      }
+
+    };
+
+    ctx.strokeStyle = getStrokeStyle(percent);
+
+    let offset = -(Math.PI / 2);
+
+    ctx.fillStyle = '#000';
+    ctx.textAlign = 'center';
+    ctx.lineWidth = 15;
+    ctx.font = '10pt verdana';
+    ctx.beginPath();
+
+    ctx.lineWidth = CustomGoogleMapMarker.size / 5;
+    ctx.arc(cw, ch, CustomGoogleMapMarker.size / 2, offset, diff + offset, false);
+    ctx.stroke();
+    ctx.fillText(this.club.name, cw, ch);
+
+    return canvas.toDataURL('bas64');
+
+  }
+
+}
